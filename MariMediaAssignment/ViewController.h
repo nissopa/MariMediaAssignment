87053fe556b3e4b009ed4aff0a2a3a62a7e3ce53
npardo <androdogs@gmail.com>
2014-01-30 18:53:51 +0200

    Initial Commit

---
+++ MariMediaAssignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MariMediaAssignment
+//
+//  Created by Nissim Pardo on 1/30/14.
+//  Copyright (c) 2014 Nissim Pardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
